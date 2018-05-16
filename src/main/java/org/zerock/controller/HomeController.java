dfd46bf12d9dc176b2fafb91758e0e197e89cc75
1027 <rana3782@gmail.com>
2018-05-16 19:45:37 +0900

    0516 gitHub practice3

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -16,8 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	//..........
-	//Hello.....2
+
+	
+	//Hello..................3
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
